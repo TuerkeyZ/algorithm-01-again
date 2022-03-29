03981db528b00e77f1a98c2afe2beaf36c59e30c
Turkeyz <2281798294@qq.com>
2022-03-29 16:27:15 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] list=new int[]{2,7,11,15,2};
+        int x=9;
+        test(list,x);
+
+    }
+
+    public static void test(int[] list, int x) {
+        for (int i = 0; i < list.length; i++) {
+            for (int j =i+1; j <list.length ; j++) {
+                int y=list[i]+list[j];
+                if(y==x)
+                {System.out.println(i);
+                 System.out.println(j);
+                 System.out.println("-------one-------");}
+            }
+
+        }
+
+
+
+    }
+
+
+}
